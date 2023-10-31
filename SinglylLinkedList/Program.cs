7a13ee0687d47f872705b9ad7ec1af3b6d39e969
BhavanaReddy99 <jorrigalabhavana01@gmail.com>
2023-10-31 11:43:44 +0530

    Add project files.

---
+++ SinglylLinkedList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglylLinkedList
+{
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        static void runApp()
+        {
+            SingleLinkedList list = new SingleLinkedList();
+            int i = 0;
+            while (i <= 10)
+            {
+                list.Add(i);
+                i++;
+            }
+               
+            // Get and print the length of the linked list
+
+                list.Display();
+                
+            
+        }
+    }
+}
+
---
+++ SinglylLinkedList/SingleLinkedList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglylLinkedList
+{
+    public class SingleLinkedList
+    {
+        public Node Head { get; set; }
+
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Node current = Head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        public int GetLength()
+        {
+            int length = 0;
+            Node current = Head;
+            while (current != null)
+            {
+                length++;
+                current = current.Next;
+            }
+            return length;
+        }
+
+        public void Display()
+        {
+            Node current = Head;
+            Console.WriteLine("Length of singly linked list = " + GetLength());
+            Console.WriteLine("Print Single linked list:");
+            while (current != null)
+            {
+                Console.WriteLine(current.Data);
+                current = current.Next;
+            }
+        }
+    }
+
+
+}
